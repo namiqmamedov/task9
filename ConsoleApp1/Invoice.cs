ebaa161bbab8985a3d5198693b1e580eede95f23
namiqmm <namigmm@code.edu.az>
2022-07-11 17:35:06 +0400

    codE

---
+++ ConsoleApp1/Invoice.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Invoice
+    {
+        private string account;
+        private string customer;
+        private string provider;
+
+        public Invoice()
+        {
+
+        }
+        public Invoice(string account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+
+            var Article = new Invoice(account, customer, provider);
+            var Quantity = new Invoice(account, customer, provider);
+        }
+
+        public string Article { get; set; }
+        public string Quantity { get; set; }
+
+        static void CostCalculation()
+        {
+            bool needEdv = true;
+            {
+                Console.WriteLine("");
+            }
+        }
+
+
+    }
+
+
+}
+
+   
+
+    
+
+
+
+
+
+
+
