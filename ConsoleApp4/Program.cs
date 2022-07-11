35146abf04b199b39556320f7915acf115ed90cc
namiqmm <namigmm@code.edu.az>
2022-07-11 17:27:03 +0400

    code

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentProgram document = new DocumentProgram();
+            document.OpenDocument();
+            document.EditDocument();
+            document.SaveDocument();
+
+
+
+        }
+
+    }
+}
