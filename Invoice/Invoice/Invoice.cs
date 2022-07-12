40de0dc50562924fffbf0493ce9427cecda30ff1
namiqmm <107476714+namiqmm@users.noreply.github.com>
2022-07-12 19:14:05 +0800

    code

---
+++ Invoice/Invoice/Invoice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Invoice
+{
+    class Invoice
+    {
+        public string account;
+        public string customer;
+        public string provider;
+
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+
+        public Invoice(string account, string customer, string provider)
+        {
+            this.account =  account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+
+        public string CostCalculation(bool needEdv)
+        {
+            if (Article == "Laptop")
+            {
+                double costOfProduct = 0;
+                if (needEdv)
+                {
+                    costOfProduct = (Quantity * 1400) + 1400 * 0.18;
+                    
+                }
+                else
+                {
+                     costOfProduct = Quantity * 1400;
+                }
+                return $"Article:{Article}, Quantity:{Quantity}, Price:{1400}, costOfProduct:{costOfProduct}";
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ Invoice/Invoice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Invoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice("3213142", "eli", "huseyn");
+            invoice.Article = "Laptop";
+            invoice.Quantity = 3;
+
+            Console.WriteLine(invoice.CostCalculation(false));
+        }
+    }
+}
